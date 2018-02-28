645bcacc484d5fe5b82c4eb2949f627e545da705
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-02-28 03:51:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; 
 
+function addElementToBeginningOfArray(arr, element){
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(arr,element){
+  
+}
+
+function addElementToEndOfArray(arr, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(arr, element){
+  
+}
